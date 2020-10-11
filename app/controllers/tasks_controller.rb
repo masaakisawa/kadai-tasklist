497b01b7b17cb54f910121cf81d6fbbd395fe422
masaaki.sawa <gomadesk@gmail.com>
2020-10-11 08:17:21 +0000

    improvement of task controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,13 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
   def index
     @task = current_user.tasks.all
   end
 
   def show
-    @task = current_user.tasks.find_by(params[:id])
+    @task = current_user.tasks.find(params[:id])
   end
 
   def new
@@ -50,5 +51,12 @@ class TasksController < ApplicationController
   
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_path
+    end
   end
 end
